fa34030c61b06ffcaadbb055c805986a02a920e6
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2025-11-22 15:14:30 +0800

    20251122上課實做

---
+++ 01-spring-xml/src/main/java/com/course/XmlMain.java
@@ -0,0 +1,12 @@
+package com.course;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XmlMain {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext();
+	}
+
+}
