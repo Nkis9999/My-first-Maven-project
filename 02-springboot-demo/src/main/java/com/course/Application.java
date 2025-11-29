d3d1f5edf28a611d95b3a7d4ca9ed56d5aaba595
DESKTOP-P4UUKAO\student <m1973lady@gmail.com>
2025-11-29 16:34:13 +0800

    99-classic-springmvc 回家做

--- 02-springboot-demo/src/main/java/com/course/Application.java
+++ 02-springboot-demo/src/main/java/com/course/Application.java
@@ -1,5 +1,10 @@
 package com.course;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +20,22 @@ public class Application {
 //		for (String name : ctx.getBeanDefinitionNames()) {
 //			System.out.println(name);
 //		}
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		
+		logger.trace("TRACE");
+		logger.debug("DEBUG");
+		logger.info("開始測試");
+		logger.warn("WARN");
+		logger.error("ERROR");
+		
+		String errerMessage = "出事了~~~~~";
+		logger.error("ERROR, {}, xxxx={}", errerMessage, "TEST");
+		
+		try {
+			FileInputStream fis = new FileInputStream("");
+		} catch (FileNotFoundException e) {
+			logger.error("FileNotFoundException");
+		}
 		
 		Toyota car = ctx.getBean(Toyota.class);
 		car.move();
